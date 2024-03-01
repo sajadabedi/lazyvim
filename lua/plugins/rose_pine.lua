245146849d5f804ce64791b2d0ec41cf8cd3260f
Sajjad Abedi <sajadxabedi@gmail.com>
2024-03-01 18:16:52 +0200

    added comment for the rose_pine

--- lua/plugins/rose_pine.lua
+++ lua/plugins/rose_pine.lua
@@ -1,3 +1,10 @@
 return {
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.g.rose_pine_variant = "moon"
+      vim.g.rose_pine_disable_background = true
+    end,
+  },
 }
